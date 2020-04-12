6374c93c8943e980fedbacf8213019b487edec88
davidjnr2 <davidasiegbu1@gmail.com>
2020-04-12 13:13:15 +0100

    challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,55 @@
-const covid19ImpactEstimator = (data) => data;
+const defaultData = {
+    region: {
+    name: "Africa",
+    avgAge: 19.7,
+    avgDailyIncomeInUSD: 5,
+    avgDailyIncomePopulation: 0.71
+    },
+    periodType: "days",
+    timeToElapse: 58,
+    reportedCases: 674,
+    population: 66622705,
+    totalHospitalBeds: 1380614
+   };
+
+const impact = (data, factor) = data.reportedCases * factor;
+const severeCases = (data, factor) = data.currentlyInfected * factor;
+const NumberOfBeds = (beds, data) = data.totalHospitalBeds - data.severeCasesByRequestedTime;
+const Cases = (cases, factor) = cases.infectionByRequestedTime;
+
+
+const estimatePeriod = (timeToElapse, estimator) => {
+    const estimateFor = periodType => () => {
+      const data = {
+        ...defaultData,
+        ...{ timeToElapse, periodType },
+      };
+      return estimator(data);
+    };
+
+  let PERIOD = {
+        DAYS: "DAYS",
+        WEEKS: "WEEKS * 7",
+        MONTHS: "MONTHS * 30"
+      };
+  
+    return {
+      days: estimateFor(PERIOD.DAYS),
+      week: estimateFor(PERIOD.WEEKS),
+      weeks: estimateFor(PERIOD.WEEKS),
+      month: estimateFor(PERIOD.MONTHS),
+      months: estimateFor(PERIOD.MONTHS)
+    };
+  };
+
+
+
+const covid19ImpactEstimator = (data) => {
+    return{
+        data,
+        impact:{},
+        severeImpact:{}
+    };
+};
 
 export default covid19ImpactEstimator;

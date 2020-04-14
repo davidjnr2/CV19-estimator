716bcc037f47f0624c7739570d8f2626e487706f
davidjnr2 <davidasiegbu1@gmail.com>
2020-04-14 23:38:20 +0100

    correct

--- src/util/index.js
+++ src/util/index.js
@@ -6,7 +6,7 @@ These and more from too many people being sick, a sizable number dying (includin
 of the best people in many fields), and many others affected by the impact of losing loved
 ones or a world operating in slow motion
 */
-
+/* eslint-disable max-len */
 /* @Project Summary  This project empowers you to attempt helping society and leaders prepare for the real big problem
 of COVID-19 pandemic, providing insights on its impact on lives, health systems, supply chains, and the economy:
 */
@@ -16,7 +16,7 @@ of COVID-19 pandemic, providing insights on its impact on lives, health systems,
 /*
    Discard the decimal part of all computations. No rounding up or down of
 figures, simply discard the decimal part.
-   @param { Number } num 
+   @param { Number } num
    @example : 3.8888888889 should be 3, not 3.88, 3.8 or 4. Also, 453325343.654
 becomes 453325343 */
 
@@ -31,62 +31,61 @@ const impactFactor = {
   mildImpact: 10,
   severeImpact: 50
 };
-
-/** 
+/* eslint-disable max-len */
+/**
     * Given the reportedCases and the multipler, this function estimates the number of currently infected
     people.
-    * @params {Number} reportedCases 
-    * @params {Number} multiplier 
-    * @returns {Number} currentlyInfected 
+    * @params {Number} reportedCases
+    * @params {Number} multiplier
+    * @returns {Number} currentlyInfected
 */
 
-const estimateInfectedPeople = (reportedCases, impactFactor) => {
-  const currentlyInfected = reportedCases * impactFactor;
+const estimateInfectedPeople = (reportedCases, factor) => {
+  const currentlyInfected = reportedCases * factor;
   return currentlyInfected;
 };
 
-
+/* eslint-disable max-len */
 /*
     @Description:  Given the parameters(currentlyInfected, duration, frequency(occurence), type(periodType), this function estimates the number of infections for a requested time period
-    
-    @params { String } type 
-    @params { Number } factor 
-    @params { Number } currentlyInfected 
-    @params { Number } duration 
-    @returns { Number }estimatedInfections 
+
+    @params { String } type
+    @params { Number } factor
+    @params { Number } currentlyInfected
+    @params { Number } duration
+    @returns { Number }estimatedInfections
 */
 
 const estimateInfectionsByRequestedTime = (currentlyInfected, frequency, periodType) => {
   let days;
-  days = timeToElapse;
   switch (periodType) {
-    case 'weeks': days * 7;
+    case 'weeks': Math.trunc(days * 7);
       break;
-    case 'months': days * 30;
+    case 'months': Math.trunc(days * 30);
       break;
-    default: days = math.trunc(timeToElapse);
+    default: return 'invalid Timespan';
   }
-  const factor = math.trunc(days / frequency);
-  const estimatedInfections = math.trunc(currentlyInfected) * (2 ** factor);
+  const factor = Math.trunc(days / frequency);
+  const estimatedInfections = Math.trunc(currentlyInfected) * (2 ** factor);
   return estimatedInfections;
 };
 
-
+/* eslint-disable max-len */
 /*
     @Description: Given the number of infectionsByRequestedTime, this function calculates the number of patients with servere positive cases
       that require hospitalization.
-    @params { Number } infectionsByRequestedTime 
-    @returns { Number } 15 % of the number of infectionsByRequestedTime 
+    @params { Number } infectionsByRequestedTime
+    @returns { Number } 15 % of the number of infectionsByRequestedTime
 */
 // eslint-disable-next-line
-const estimateSeverePositiveCaseRequireHospitalization = (infectionsByRequestedTime) => math.trunc(infectionsByRequestedTime) * 0.15;
+const estimateSeverePositiveCaseRequireHospitalization = (infectionsByRequestedTime) => Math.trunc(infectionsByRequestedTime) * 0.15;
 
-
+/* eslint-disable max-len */
 /*
     @Description: Given the severeCasesByRequestedTime, totalHospitalBeds, this function estimates the available hospital beds(availableBeds) for severe COVID-19 positive patients.
-    @params { Number } severeCasesByRequestedTime 
-    @params { Number } totalHospitalBeds 
-    @returns { Number } availableBeds 
+    @params { Number } severeCasesByRequestedTime
+    @params { Number } totalHospitalBeds
+    @returns { Number } availableBeds
     Positive indicates availability while negative indicates shortages.
 */
 
@@ -94,21 +93,20 @@ const estimateHospitalBedsByRequestedTime = (severeCasesByRequestedTime, totalHo
   // const requiredNumberOfBeds = parseFloat(severeCasesByRequestedTime);
   const expectedBedsAvailable = totalHospitalBeds * 0.35;
   const occupied = totalHospitalBeds * 0.65;
-  const availableBeds = math.trunc(expectedBedsAvailable - severeCasesByRequestedTime);
+  const availableBeds = Math.trunc(expectedBedsAvailable - severeCasesByRequestedTime);
   if (availableBeds < 0) {
     return computeToWholeNumber(occupied);
   }
-  else {
-    return computeToWholeNumber(availableBeds);
-  }
+
+  return computeToWholeNumber(availableBeds);
 };
 
 
 /*
     @Description: Given the infectionsByRequestedTime, this function estimates the number of COVID-19 infected patients
       that would require ICU care
-    @params { Number } infectionsByRequestedTime 
-    @returns { Number } numberOfICUCareCases 
+    @params { Number } infectionsByRequestedTime
+    @returns { Number } numberOfICUCareCases
 */
 
 const estimateICUCareCases = (infectionsByRequestedTime) => {
@@ -120,8 +118,8 @@ const estimateICUCareCases = (infectionsByRequestedTime) => {
 /*
     @Description: Given the infectionsByRequestedTime, this function estimates the number of severe positive
 cases that will require ventilators.
-    @params { Number } infectionsByRequestedTime 
-    @returns { Number } numberOfVentilatorCases 
+    @params { Number } infectionsByRequestedTime
+    @returns { Number } numberOfVentilatorCases
 */
 
 const estimateVentilatorCases = (infectionsByRequestedTime) => {
@@ -129,29 +127,27 @@ const estimateVentilatorCases = (infectionsByRequestedTime) => {
   return computeToWholeNumber(numberOfVentilatorCases);
 };
 
-
+/* eslint-disable max-len */
 /*
     @Description: Given the infectionsByRequestedTime, avgDailyIncomeInUSD, avgDailyIncomePopulation, duration, type, this function estimates the amount an economy losses over a period of time
       in relation to infected patients
-    @params { Number } infectionsByRequestedTime 
-    @params { Number } avgDailyIncomePopulation 
-    @params { Number } avgDailyIncomeInUSD 
+    @params { Number } infectionsByRequestedTime
+    @params { Number } avgDailyIncomePopulation
+    @params { Number } avgDailyIncomeInUSD
     @params { String } type  values : 'days' || 'week'|| 'weeks' || 'month' ||'months'
-    @returns { Number } numberOfVentilatorCases 
+    @returns { Number } numberOfVentilatorCases
 */
 
 // eslint-disable-next-line
 const estimateEconomyLoss = (infectionsByRequestedTime, avgDailyIncomeInUSD, avgDailyIncomePopulation, periodType) => {
   let days;
-  days = timeToElapse;
   switch (periodType) {
-    case 'weeks': days * 7;
+    case 'weeks': Math.trunc(days * 7);
       break;
-    case 'months': days * 30;
+    case 'months': Math.trunc(days * 30);
       break;
-    default: days = math.trunc(timeToElapse);
+    default: return 'invalid Timespan';
   }
-
   // eslint-disable-next-line
   const estimateEconomyLoss = (infectionsByRequestedTime * avgDailyIncomeInUSD * avgDailyIncomePopulation) / days;
   return computeToWholeNumber(estimateEconomyLoss);

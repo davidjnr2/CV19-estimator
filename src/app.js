d8c50327e7487d032b38711540de4c61e4e62e46
Obinna Asiegbu [MTN Nigeria - CS] <obinnaas@mtn.com.ng>
2020-04-15 02:55:18 +0100

    frontend

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const menuBtn = document.getElementById('menuBtn');
+const sideNav = document.getElementById('sideNav');
+
+sideNav.style.right = '-250px';
+menuBtn.addEventListener('click', () => {
+  if (sideNav.style.right == '-250px') {
+    sideNav.style.right = '0';
+  } else {
+    sideNav.style.right = '-250px';
+  }
+});
